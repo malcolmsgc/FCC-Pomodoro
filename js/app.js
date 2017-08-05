d4cdee8460cc89f3685335ad2fd06e3c4e598073
Malcolm Cumming <bemjogar@gmail.com>
2017-08-05 22:05:31 +0100

    CDT class handles padding and values over 3 digits

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ class CountDownTimer {
 
     constructor() {
         this.timerIntervalID; //var used to clear interval
+        this.numDisplaySlots; //used to check how much padding is required
     }
 
     setInterval() {
@@ -13,7 +14,7 @@ class CountDownTimer {
     startCountDown(secs) {
         const   now = Date.now(),
                 timeAtEnd = now + (secs * 1000); //convert secs to milliseconds
-        this.displayCountDown(secs);
+        this._displayCountDown(secs);
         this.timerIntervalID = setInterval(
             () => {
                 const secsRemaining = Math.round((timeAtEnd - Date.now()) / 1000);
@@ -21,18 +22,53 @@ class CountDownTimer {
                     clearInterval(this.timerIntervalID)
                     return;
                 }
-                console.log(secsRemaining);
+                this._displayCountDown(secsRemaining);
                 }
         , 1000) //run every second to update display
     }
 
-    displayCountDown(secs) {
+    _displayCountDown(seconds, {noNeg , padTo, returnAsNum} = {noNeg: true, padTo: 2, returnAsNum: false}) {
         //tie this into UI
-        console.log(secs);
-        return secs;
+        let mins = Math.floor(seconds / 60),
+            secs = seconds % 60,
+            padMinsTo = this.numDisplaySlots || padTo;
+        //if statement runs on first occasion
+        //js allows more than two display slots. UI may or may not support more than 2
+        if (mins > 60 && !(this.numDisplaySlots)) {
+            console.warn("No support for hours. Minutes exceed 1 hour.");
+            padMinsTo = (mins.toString().length < padTo) ? padTo : mins.toString().length;
+            console.log(padMinsTo);
+            this.numDisplaySlots = padMinsTo; // numDisplaySlots used as flag to run if statement
+            console.log(this.numDisplaySlots);
+        }
+        if (noNeg) {
+            mins = Math.abs(mins);
+            secs = Math.abs(secs);
+        }
+        secs = this._padNum(secs, padTo);
+        mins = this._padNum(mins, padMinsTo);
+        console.log({mins, secs});
+        if (returnAsNum) {
+            secs = parseFloat(secs);
+            mins = parseFloat(mins);
+        }
+        return {mins, secs};
     }
+
+    _padNum(num, padDepth = 2) {
+        if (typeof num === "number") {
+            num = num.toString();
+        }
+        while (num.length < padDepth) {
+            num = `0${num}`;
+        }
+        return num;
+    }
+        
 }
 
+/* -------------------------------- */
+/* ENDOF COUNTDOWNTIMER CLASS       */
 /* -------------------------------- */
 
 
@@ -40,4 +76,4 @@ class CountDownTimer {
 
 
 
-const countDown = new CountDownTimer();
+const pomodoro = new CountDownTimer();
