d08f020c31c070fbc563cbbc4a0b692fdc9cd361
Malcolm Cumming <bemjogar@gmail.com>
2017-08-06 19:06:18 +0100

    Basic countdown shows in UI

--- js/app.js
+++ js/app.js
@@ -6,17 +6,31 @@ class CountDownTimer {
     constructor() {
         this.timerIntervalID; //var used to clear interval
         this.numDisplaySlots; //used to check how much padding is required
+        
     }
 
-    setInterval() {
+    _setNodeRefs(selectorsObj = { 
+                                timerRef: ".count-down" ,
+                                minsRef: ".count-down .mins",
+                                secsRef: ".count-down .secs"
+                            }) {
+        const selectors = Object.keys(selectorsObj);
+        selectors.forEach(
+            (key) => this[key] = Array.from(document.querySelectorAll(selectorsObj[key]))
+        );
+        return selectors;
     }
+
+
 
     startCountDown(secs) {
         clearInterval(this.timerIntervalID);
+        const nodeRefs = this._setNodeRefs();
+        
         this.numDisplaySlots = undefined;
         const   now = Date.now(),
                 timeAtEnd = now + (secs * 1000); //convert secs to milliseconds
-        this._displayCountDown(secs);
+        this._displayCountDown(secs, {nodeRefs});
         this.timerIntervalID = setInterval(
             () => {
                 const secsRemaining = Math.round((timeAtEnd - Date.now()) / 1000);
@@ -24,13 +38,16 @@ class CountDownTimer {
                     clearInterval(this.timerIntervalID);
                     return;
                 }
-                this._displayCountDown(secsRemaining);
+                this._displayCountDown(secsRemaining, {nodeRefs});
                 }
         , 1000) //run every second to update display
     }
 
-    _displayCountDown(seconds, {noNeg , padTo, returnAsNum} = {noNeg: true, padTo: 2, returnAsNum: false}) {
-        //tie this into UI
+    _displayCountDown(seconds, {nodeRefs, noNeg , padTo, returnAsNum} = {
+        nodeRefs: [],
+        noNeg: true,
+        padTo: 2,
+        returnAsNum: false}) {
         let mins = Math.floor(seconds / 60),
             secs = seconds % 60,
             padMinsTo = this.numDisplaySlots || padTo; //takes value of max slots needed for minutes
@@ -50,6 +67,7 @@ class CountDownTimer {
         secs = this._padNum(secs, padTo);
         mins = this._padNum(mins, padMinsTo);
         console.log({mins, secs});
+        this._handleSlots({mins, secs}, nodeRefs);
         if (returnAsNum) {
             secs = parseFloat(secs);
             mins = parseFloat(mins);
@@ -65,6 +83,36 @@ class CountDownTimer {
             num = `0${num}`;
         }
         return num;
+    }
+
+    _handleSlots(timeDigitObj = {}, nodeRefs) {
+        // destructure node refs into default vars
+        // and pull in nodes held on class object
+        // as arrays of nodes
+        const   [timerRef, minsRef, secsRef] = nodeRefs,
+                timerNodes = this[timerRef],
+                minsNodes = this[minsRef],
+                secsNodes = this[secsRef];
+        //split time strings into arrays
+        let { mins, secs } = timeDigitObj;
+        mins = mins.split('');
+        secs = secs.split('');
+        //get nodes
+        const callback = (nodeArray, timeArray) => { nodeArray.forEach( 
+            (node, i) => {
+                let time = timeArray[i];
+                let timeSlotCurrent = node.childNodes[0];
+                let timeSlotNew = node.childNodes[1];
+                console.log({timeSlotCurrent, time})
+                timeSlotCurrent.textContent = time;
+            }
+        )};
+
+        callback(secsNodes, secs);
+        callback(minsNodes, mins);
+       //console.log({secs, secsNodes});
+
+
     }
 }
 
